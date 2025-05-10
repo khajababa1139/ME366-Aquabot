bc6be7cd9a1403f4f66e95a9e448eaee62f58941
Estyak Ahmed <84046929+khajababa1139@users.noreply.github.com>
2025-05-11 02:31:30 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import subprocess
 
 def main():
-    # Run the first Python file in the 'components' folder
     subprocess.run(["python", "components/server.py"])
-
-    # Run the second Python file in the 'components' folder
     subprocess.run(["python", "components/rpy.py"])
 
     print("Both files have finished executing.")
